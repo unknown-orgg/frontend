fa1100e2abe38700b0b3493dd167f8d895395543
rodrigoRolim <rodrigorolimveras92@gmail.com>
2022-10-27 12:41:25 -0300

    feat(global): create new global types and interfaces
    types to define design system of the component's api

--- products/unknow/src/types.d.ts
+++ products/unknow/src/types.d.ts
@@ -1,2 +1,20 @@
 type Label = string | number;
+type Content = string | number;
+type Email = string;
+type Phone = string;
+type LabName = string;
+type Address = string;
+type Direction = 'horizontal' | 'vertical';
 type Size = "xsmall" | "small" | "medium" | "large" | "xlarge";
+type Color = "success" | "warning" | "danger" | "primary";
+
+interface iContacts {
+  email: Email;
+  phone: Phone;
+}
+
+interface iLab {
+  name: LabName;
+  address: Address;
+  contacts: iContacts;
+}
