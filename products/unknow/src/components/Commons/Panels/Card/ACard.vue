1b3d62470d28fdb329e7c52f978436c46bd88074
rodrigoRolim <rodrigorolimveras92@gmail.com>
2022-11-15 02:34:58 -0300

    test: create tests cases to card component

--- products/unknow/src/components/Commons/Panels/Card/ACard.vue
+++ products/unknow/src/components/Commons/Panels/Card/ACard.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="ACard box-shadow">
+  <div 
+    class="ACard card bg-white" 
+    data-testid="ACard"
+  >
     <slot></slot>
   </div>
 </template>
 <style lang="scss" scoped>
 .ACard {
-
+  width: 100%;
 }
 </style>
