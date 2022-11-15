bde8cd079e75b15d3c0e7dd2a446424ccf86b41b
rodrigoRolim <rodrigorolimveras92@gmail.com>
2022-11-15 02:35:37 -0300

    fix: define a style to lab list

--- products/unknow/src/components/Labs/LabList.vue
+++ products/unknow/src/components/Labs/LabList.vue
@@ -19,3 +19,16 @@ interface iPropsLabList {
 }
 defineProps<iPropsLabList>();
 </script>
+<style lang="scss" scoped>
+.LabList {
+  width: 100%;
+  display: grid;
+  grid-template-columns: auto auto;
+  column-gap: 1rem;
+  row-gap: 1rem;
+  & .LabList__Item {
+    width: 100%;
+    
+  }
+}
+</style>
